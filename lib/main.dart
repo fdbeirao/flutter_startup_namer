830b93827017b3fdb0f6208e880558eaa391232f
Fábio Beirão <fdbeirao@gmail.com>
2018-04-21 13:54:30 +0200

    Set the theme's primary color as white

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords(),
     );
   }
